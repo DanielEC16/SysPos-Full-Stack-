1eda7679c7ecbac18c0447eb2ee7a819b8dc09a9
DanielEC16 <danielcairo695@gmail.com>
2025-01-15 19:55:18 -0500

    Seccion de clientes add,delete,post,put completada

--- SysPOS (BACK-END)/src/main/java/aplicaciones/spring/controlador/ProductController.java
+++ SysPOS (BACK-END)/src/main/java/aplicaciones/spring/controlador/ProductController.java
@@ -8,8 +8,11 @@ import java.nio.file.Paths;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +27,8 @@ import aplicaciones.spring.modelo.Cliente;
 import aplicaciones.spring.modelo.Producto;
 import aplicaciones.spring.servicios.CategoriaService;
 import aplicaciones.spring.servicios.ProductoService;
+import io.micrometer.core.ipc.http.HttpSender.Response;
+
 import org.springframework.web.bind.annotation.RequestBody;
 
 
@@ -35,14 +40,30 @@ public class ProductController {
 	@Autowired
 	CategoriaService categoriaService;
 
+	@PostMapping("/guardar")
+	public Producto guardarProducto(@RequestBody Producto producto) {
+		return productoService.guardar(producto);
+	}
+	
 	@GetMapping("/listar")
 	public List<Producto> listarProductos() {
 		return productoService.listar();
 	}
 	
-	@PostMapping("/guardar")
-	public Producto guardarProducto(@RequestBody Producto producto) {
-		return productoService.guardar(producto);
+	@GetMapping("/listar/{id}")
+	public ResponseEntity<Producto> ObtenerProduct(@PathVariable Long id){
+		Producto producto = productoService.obtenerPorId(id);
+		if (producto != null ) {
+			return new ResponseEntity<>(producto,HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	@DeleteMapping("/eliminar/{id}")
+	public String eliminarProducto(@PathVariable Long id) {
+		productoService.eliminar(id);
+		return "Producto borrado exitosamente";
 	}
 	
 
--- SysPOS (BACK-END)/src/main/java/aplicaciones/spring/servicios/ProductoService.java
+++ SysPOS (BACK-END)/src/main/java/aplicaciones/spring/servicios/ProductoService.java
@@ -5,34 +5,47 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import aplicaciones.spring.modelo.Categoria;
 import aplicaciones.spring.modelo.Producto;
+import aplicaciones.spring.repositorios.ICategoria;
 import aplicaciones.spring.repositorios.IProducto;
 
 @Service("producto")
 public class ProductoService {
 	@Autowired
 	IProducto iProducto;
-	public List<Producto> listar(){
+	@Autowired
+	ICategoria iCategoria;
+
+	public List<Producto> listar() {
 		return iProducto.findAll();
 	}
+
 	public Producto guardar(Producto producto) {
+		Categoria categoria = iCategoria.findById(producto.getCategoria().getId())
+				.orElseThrow(() -> new RuntimeException("Categoría no encontrada"));
+		producto.setCategoria(categoria);
 		return iProducto.save(producto);
 	}
+
 	public Producto obtenerPorId(Long id) {
 		return iProducto.findById(id).orElse(null);
 	}
+
 	public void eliminar(Long id) {
 		iProducto.deleteById(id);
 	}
-	
+
 	public List<Producto> buscarProductos(String keyword) {
-        return iProducto.buscarPorCodigoONombre(keyword);
-    }
+		return iProducto.buscarPorCodigoONombre(keyword);
+	}
+
 	public Producto buscarProductoPorCodigo(String codigo) {
 		return iProducto.findByCodigo(codigo);
 	}
+
 	public List<Producto> obtenerProductosPorIds(List<Long> productosIds) {
-        return iProducto.findAllById(productosIds);  // Consulta por lista de IDs
-    }
-	
+		return iProducto.findAllById(productosIds); // Consulta por lista de IDs
+	}
+
 }
